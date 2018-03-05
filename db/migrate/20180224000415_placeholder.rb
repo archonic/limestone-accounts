10f80ee3b51cfd545a3c09ff17d2015f8e2bbe12
Joshua Flark <joshuajmark@gmail.com>
2018-03-05 15:46:15 -0800

    Get tests passing on codeship

---
+++ db/migrate/20180224000415_placeholder.rb
@@ -0,0 +1,10 @@
+class Placeholder < ActiveRecord::Migration[5.2]
+  def change
+    # ... *tumbleweed*...
+
+    # This file is in place only to satisfy rake migration status.
+    # Migrations are for sharing changes a database over time.
+    # Since this is a boilerplate, we only share the creation snapshot, which is schema.rb
+    # Use rake:db:setup to create and seed a new env.
+  end
+end
