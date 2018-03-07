fa303ebf99a01a140070396c61c8949372691858
Joshua Flark <joshuajmark@gmail.com>
2018-03-07 11:50:19 -0800

    Webpacker 3.3.0 working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,31 @@
 const { environment } = require('@rails/webpacker')
+const coffee =  require('./loaders/coffee')
+const webpack = require('webpack')
 
+// Get a pre-configured plugin
+const manifestPlugin = environment.plugins.get('Manifest')
+manifestPlugin.opts.writeToFileEmit = false
+
+// Add an additional plugin of your choosing : ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': 'tether',
+    Popper: ['popper.js', 'default'],
+    ActionCable: 'actioncable'
+  })
+)
+
+// Insert before a given plugin
+environment.plugins.insert('CommonChunkVendor',
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor', // Vendor code
+    minChunks: (module) => module.context && module.context.indexOf('node_modules') !== -1
+  })
+, { before: 'manifest' })
+
+environment.loaders.append('coffee', coffee)
 module.exports = environment
