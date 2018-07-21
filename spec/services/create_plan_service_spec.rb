366489acac63d6d6460e30cad59f00224ec6b5dc
Joshua Flark <joshuajmark@gmail.com>
2018-07-21 10:19:57 -0700

    Don't need this

--- spec/services/create_plan_service_spec.rb
+++ spec/services/create_plan_service_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 require 'stripe_mock'
 
 RSpec.describe CreatePlanService, type: :service do
-  # let(:stripe_helper) { StripeMock.create_test_helper }
   let(:plan) { create(:plan) }
   before do
     allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('development'))
