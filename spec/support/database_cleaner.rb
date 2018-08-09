a33c566b7697393aef8b63a78cac761613ddfbd2
Joshua Flark <joshuajmark@gmail.com>
2018-08-08 20:07:21 -0700

    shutup database cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,4 +1,7 @@
 RSpec.configure do |config|
+  DatabaseCleaner.allow_production = false
+  DatabaseCleaner.allow_remote_database_url = true
+
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
